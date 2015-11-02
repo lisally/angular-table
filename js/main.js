575a2415235beaa9f930c1012f698d07c6fb2235
Sally Li <lisally@uw.edu>
2015-11-02 10:13:55 -0800

    after second class

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ sortApp.controller('mainController', function($scope, $http){
     $scope.sortType = 'name'
     $http.get('data/salaries.json').then(function(response) {
         $scope.data = response.data     
-        //console.log($scope.data)
+        console.log($scope.data)
     })
 })
 
